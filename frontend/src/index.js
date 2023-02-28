69f913348786e205027294e5a303ec0523020380
Igor Boyko <iboyko@gmail.com>
2023-02-28 11:24:04 -0800

    cleaned up index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
@@ -9,7 +9,6 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById('root')
   );
-
 
   // const root = ReactDOM.createRoot(document.getElementById('root'));
   // root.render(
