de99cc0959d83ea43ec12566b4c36e1e9649f717
iboyko21 <iboyko@gmail.com>
2023-09-28 12:21:30 -0700

    cleaned up comments

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,25 +1,3 @@
-// // import React, { StrictMode } from 'react';
-// import createRoot from "react-dom/client";
-// // import ReactDOM from 'react-dom';
-// import App from './App';
-// import { BrowserRouter } from "react-router-dom";
-
-// // ReactDOM.render(
-// //   <BrowserRouter>
-// //     <App />
-// //   </BrowserRouter>,
-// //   document.getElementById('root')
-// //   );
-
-//   const root = createRoot(document.getElementById('root'));
-//   root.render(
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   );
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
